06c961e30f313d0d2c79f058f1213fb52e095b25
kuzhanthaivelu <113583512+kuzhanthaivel@users.noreply.github.com>
2023-03-29 16:47:58 +0530

    commit

--- crowdfunding_contracts/contracts/Contract.sol
+++ crowdfunding_contracts/contracts/Contract.sol
@@ -2,53 +2,79 @@
 pragma solidity ^0.8.9;
 
 contract Crowdfunding {
-    struct Campaign{
+
+    struct Campaign {
         address owner;
         string title;
-        string descrition;
+        string description;
         uint target;
         uint amountCollected;
+        uint deadline;
         string image;
         address[] donators;
         uint[] donations;
     }
+
     mapping (uint => Campaign) public campaigns;
 
     uint public numberOfCampaigns = 0;
 
     function createCampaign(
-        string memory _title,
-        string memory _description,
-        address owner,
-        uint _target,
-        uint _amountCollected,
-        uint _deadline,
+        address _owner, 
+        string memory _title, 
+        string memory _description, 
+        uint _target, 
+        uint _amountCollected, 
+        uint _deadline, 
         string memory _image
-    ) public returns (uint){
+     ) 
+     
+        public returns (uint) {
         Campaign storage campaign = campaigns[numberOfCampaigns];
 
-        require(
-            campaign.deadline > block.timestamp,
-            "deadline must be in the future"
+        require(campaign.deadline > block.timestamp, 
+        "deadline must be in future"
         );
 
         campaign.owner = _owner;
         campaign.title = _title;
-        campaign.descrition = _description;
+        campaign.description = _description;
         campaign.amountCollected = _amountCollected;
-        campaign.target =_target;
+        campaign.target = _target;
         campaign.deadline = _deadline;
         campaign.image = _image;
 
         numberOfCampaigns++;
+        return (numberOfCampaigns-1);
+     }
 
-        return(numberOfCampaigns -1);
+    function donateToCampaign(uint _id) public payable {
+        uint amount = msg.value; //msg.value means amount sending by the donators
+        Campaign storage campaign = campaigns[_id];
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent, ) = payable(campaign.owner).call{value: amount}("");
+        if(sent){
+
+            campaign.amountCollected=campaign.amountCollected + amount;
+        }
     }
 
-    function donateToCampaign() public {}
+    function getDonators(uint _id) public view returns(address[] memory, uint[] memory){
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
 
-    function getDonators() public {}
-    
-    function getCampaign() public {}
+    function getCampaign() public view returns(Campaign[] memory){
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+        for(uint i=0; i < numberOfCampaigns ; i++){
+        Campaign storage item = campaigns[i];
+        allCampaigns[i] = item;
+        }
+        return allCampaigns;
+
+    }
+
+
 
 }
